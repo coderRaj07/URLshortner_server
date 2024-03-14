5848fd918ef4792c1c5021be85a24d3c6f8ca6e4
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2024-03-14 16:09:34 +0530

    feat(index.js): endpoint for cronjobs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http");
 const express = require("express");
+const favicon = require('express-favicon');
+const dotenv = require("dotenv");
 const cron = require("node-cron");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
-const favicon = require('express-favicon');
-const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -17,9 +17,7 @@ connectToMongoDB(process.env.DB).then(() =>
 );
 
 app.use(favicon(__dirname + 'icons8-favicon-16.png'));
-
 app.use(express.json());
-
 app.use("/url", urlRoute);
 
 app.get("/:shortId", async (req, res) => {
@@ -42,21 +40,28 @@ app.get("/:shortId", async (req, res) => {
   res.redirect(entry.redirectURL);
 });
 
+// Define cron job and endpoint for triggering it
+cron.schedule("*/5 * * * *", async () => {
+  const now = new Date();
+  const urls = await URL.find({ expirationDuration: { $ne: null } });
+
+  urls.forEach(async (url) => {
+    const creationTime = url.createdAt;
+    if (now - creationTime > 30 * 60 * 1000) {
+      // Delete the URL if it's 30 minutes or older
+      await URL.deleteOne({ _id: url._id });
+      console.log(`Expired URL ${url.shortId} deleted`);
+    }
+  });
+});
+
+app.post("/cronjobs/start", (req, res) => {
+  // You can use this endpoint to trigger the cron job manually if needed
+  res.json({ message: "Cron job started" });
+});
+
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server Started at PORT:${PORT}`);
-  cron.schedule("*/5 * * * *", async () => {
-    const now = new Date();
-    const urls = await URL.find({ expirationDuration: { $ne: null } });
-
-    urls.forEach(async (url) => {
-      const creationTime = url.createdAt;
-      if (now - creationTime > 30 * 60 * 1000) {
-        // Delete the URL if it's 30 minutes or older
-        await URL.deleteOne({ _id: url._id });
-        console.log(`Expired URL ${url.shortId} deleted`);
-      }
-    });
-  });
 });
